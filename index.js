69092022cd166d1bca6f4ae67ff604195f45b404
thembz12 <oyin1239@gmail.com>
2024-06-15 18:05:25 +0100

    my sever

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require ("express")
-const app = express()
-const PORT = 3232
+// const express = require ("express")
+// const app = express()
+// const PORT = 3232
 
 
-app.get("/", (req,res)=>{
-    try{
-    res.send('GOD IS THE GREATEST')
-    }catch(error){
-        console.log(err.message);
-    }
-})
+// app.get("/", (req,res)=>{
+//     try{
+//     res.send('GOD IS THE GREATEST')
+//     }catch(error){
+//         console.log(err.message);
+//     }
+// })
 
 app.listen(PORT,()=>{
     console.log(`server is listening to ${PORT}`);
